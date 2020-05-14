e1cc405e46b95817a8bb31bb04f051cdb2e81c4e
tiagon <radek.wojciechowski1997@gmail.com>
2020-05-14 20:22:03 +0200

    Updates

---
+++ precache-manifest.8df8feb332a43abe99c9d8883b297fe4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "22020db9695a1d084d3c",
+    "url": "/weather-app/static/js/main.c7404bd0.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "f49d9d00d582da4051b4",
+    "url": "/weather-app/static/js/2.1033dfc4.chunk.js"
+  },
+  {
+    "revision": "4b2064f762bf7917e69e8ffe00c92481",
+    "url": "/weather-app/index.html"
+  }
+];
